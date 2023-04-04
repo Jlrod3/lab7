d0e471028093598e738df33fbe929c34ca8f4717
Jlrod3 <112913660+Jlrod3@users.noreply.github.com>
2023-04-04 16:43:59 -0500

    js setting up variables and functions

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function init(){
     //add your javascrip between these two lines of code
-     
+    var click = document.getElementById("entrybutton");
     
+    function outputtext(){
+
+    }
     
-    
-    
-    
-    
-    
+    click.addEventListener('click', outputtext);
+
+    }    
     window.addEventListener('load', init);
